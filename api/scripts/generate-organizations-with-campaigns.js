9b77ee0390bc8bd8f0f45ab03e26cccf5011c5f3
Jonas Grillères <grilleres.j@gmail.com>
2020-07-22 16:10:50 +0200

    Add script for orga

--- api/scripts/generate-organizations-with-campaigns.js
+++ api/scripts/generate-organizations-with-campaigns.js
@@ -1,25 +1,28 @@
 const { knex } = require('../db/knex-database-connection');
 const { _do } = require('./generate-campaign-with-participants');
+const faker = require('faker');
 const _ = require('lodash');
 
 async function main() {
+  try {
+    const organizationCount = process.argv[2];
 
-  const organizationCount = process.argv[2];
+    for (let index = 0; index < organizationCount; index++) {
+      const [organizationId] = await knex('organizations').
+        returning('id').
+        insert({ name: `Organization ${index}`, type: 'SCO', isManagingStudents: true });
 
-  return _.times(parseInt(organizationCount), async function(index) {
-
-    try {
-      console.log('Before');
-      const [organizationId] = await knex('organizations')
-        .returning('id')
-        .insert({ name: `Organization ${index}`, type: 'SCO', isManagingStudents: true });
-      console.log('organizationId', organizationId);
+      const firstName = faker.name.firstName();
+      const lastName = faker.name.firstName();
+      const [id] = await knex('users').returning('id').insert({ firstName, lastName, email: `${firstName}.${lastName}@example.net`, password: '' });
+      await knex('memberships').insert({ userId: id, organizationId, organizationRole: 'ADMIN' });
 
       await _do({ organizationId, participantCount: 1000, profileType: 'medium', campaignType: 'assessment' });
-    } catch (e) {
-      console.log(e);
     }
-  });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 }
 
 if (require.main === module) {
