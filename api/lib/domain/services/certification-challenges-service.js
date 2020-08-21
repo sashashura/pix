dc6659410a5d3e5b43175f492152e9ce77c19e74
Etienne GIROT <egirot@octo.com>
2020-08-21 17:58:41 +0200

    TO DELETE : Scratch refactoring (that passes tests)

--- api/lib/domain/services/certification-challenges-service.js
+++ api/lib/domain/services/certification-challenges-service.js
@@ -5,94 +5,166 @@ const {
 } = require('../constants');
 
 const KnowledgeElement = require('../models/KnowledgeElement');
-const UserCompetence = require('../models/UserCompetence');
-const Challenge = require('../models/Challenge');
 const challengeRepository = require('../../infrastructure/repositories/challenge-repository');
 const competenceRepository = require('../../infrastructure/repositories/competence-repository');
 const answerRepository = require('../../infrastructure/repositories/answer-repository');
 const knowledgeElementRepository = require('../../infrastructure/repositories/knowledge-element-repository');
 
+function _correctAnswerIds(knowledgeElementsByCompetence) {
+  const knowledgeElements = KnowledgeElement.findDirectlyValidatedFromGroups(knowledgeElementsByCompetence);
+  const answerIds = _.map(knowledgeElements, 'answerId');
+  return answerIds;
+}
+
 module.exports = {
 
   async pickCertificationChallenges(placementProfile) {
     const knowledgeElementsByCompetence = await knowledgeElementRepository
-      .findUniqByUserIdGroupedByCompetenceId({ userId: placementProfile.userId, limitDate: placementProfile.profileDate });
-
-    const knowledgeElements = KnowledgeElement.findDirectlyValidatedFromGroups(knowledgeElementsByCompetence);
-    const answerIds = _.map(knowledgeElements, 'answerId');
-
+      .findUniqByUserIdGroupedByCompetenceId({
+        userId: placementProfile.userId,
+        limitDate: placementProfile.profileDate
+      });
+    const answerIds = _correctAnswerIds(knowledgeElementsByCompetence);
     const challengeIdsCorrectlyAnswered = await answerRepository.findChallengeIdsFromAnswerIds(answerIds);
 
     const allChallenges = await challengeRepository.findFrenchFranceOperative();
     const pixCompetences = await competenceRepository.listPixCompetencesOnly();
-    const pixCompetenceIds = pixCompetences.map((pixCompetence) => pixCompetence.id);
 
-    const pixOperativeChallengesCorrectlyAnswered = _(challengeIdsCorrectlyAnswered)
-      .map((challengeId) => Challenge.findById(allChallenges, challengeId))
-      .compact()
-      .filter((challenge) => pixCompetenceIds.includes(challenge.competenceId))
-      .value();
-
-    pixOperativeChallengesCorrectlyAnswered.forEach((challenge) => {
-
-      const userCompetence = _getUserCompetenceByChallengeCompetenceId(placementProfile.userCompetences, challenge);
-
-      if (!userCompetence.isCertifiable()) {
-        return;
-      }
-
-      challenge.skills
-        .filter((skill) => _skillHasAtLeastOneChallenge(skill, allChallenges))
-        .forEach((publishedSkill) => userCompetence.addSkill(publishedSkill));
+    const learningContentForCertificationCandidate = LearningContentForCertificationCandidate.from({
+      allFrenchFranceOperativeChallenges: allChallenges,
+      pixCompetences,
+      allCorrectlyAnsweredChallengeIds: challengeIdsCorrectlyAnswered,
+      placementProfile
     });
-
-    const userCompetences = UserCompetence.orderSkillsOfCompetenceByDifficulty(placementProfile.userCompetences);
-    let certificationChallengesByCompetence = {};
-
-    userCompetences.forEach((userCompetence) => {
-      userCompetence.skills.forEach((skill) => {
-        if (!_hasCompetenceEnoughCertificationChallenges(userCompetence.id, certificationChallengesByCompetence)) {
-          const challengesToValidateCurrentSkill = Challenge.findBySkill({ challenges: allChallenges, skill });
-          const challengesLeftToAnswer = _.difference(challengesToValidateCurrentSkill, pixOperativeChallengesCorrectlyAnswered);
-
-          const challengesPoolToPickChallengeFrom = (_.isEmpty(challengesLeftToAnswer)) ? challengesToValidateCurrentSkill : challengesLeftToAnswer;
-          const challenge = _.sample(challengesPoolToPickChallengeFrom);
-
-          const certificationChallenge = new CertificationChallenge({
-            challengeId: challenge.id,
-            competenceId: skill.competenceId,
-            associatedSkillName: skill.name,
-            associatedSkillId: skill.id
-          });
-          certificationChallengesByCompetence = _addUniqueCertificationChallengeForCompetence(certificationChallengesByCompetence, certificationChallenge);
-        }
-      });
-    });
-
-    return _.flatten(Object.values(certificationChallengesByCompetence));
+    return learningContentForCertificationCandidate.pickCertificationChallenges();
   },
 };
 
-function _hasCompetenceEnoughCertificationChallenges(competenceId, certificationChallengesByCompetence) {
-  const certificationChallengesForGivenCompetence = certificationChallengesByCompetence[competenceId] || [];
-  return certificationChallengesForGivenCompetence.length >= MAX_CHALLENGES_PER_SKILL_FOR_CERTIFICATION;
-}
-
-function _addUniqueCertificationChallengeForCompetence(certificationChallengesByCompetence, certificationChallenge) {
-  const mutatedCertificationChallengesByCompetence = _.cloneDeep(certificationChallengesByCompetence);
-  const certificationChallengesForGivenCompetence = mutatedCertificationChallengesByCompetence[certificationChallenge.competenceId] || [];
-  if (!_.some(certificationChallengesForGivenCompetence, { challengeId: certificationChallenge.challengeId })) {
-    certificationChallengesForGivenCompetence.push(certificationChallenge);
+class LearningContentForCertificationCandidate {
+  constructor(learningContent) {
+    this._learningContent = learningContent;
   }
-  mutatedCertificationChallengesByCompetence[certificationChallenge.competenceId] = certificationChallengesForGivenCompetence;
-  return mutatedCertificationChallengesByCompetence;
+
+  pickCertificationChallenges() {
+    const pickedCertificationChallengesByCompetenceId = {};
+    _.forEach(this._learningContent, (skills, competenceId) => {
+      skills.forEach((skill) => {
+        const alreadyPickedChallengesByCompetence = pickedCertificationChallengesByCompetenceId[competenceId] || [];
+        if (alreadyPickedChallengesByCompetence.length < MAX_CHALLENGES_PER_SKILL_FOR_CERTIFICATION) {
+          const pickedChallenge = this._pickRandomChallengeForSkill(skill);
+          if (!this._hasChallengeAlreadyBeenPicked(alreadyPickedChallengesByCompetence, pickedChallenge)) {
+            alreadyPickedChallengesByCompetence.push(
+              new CertificationChallenge({
+                challengeId: pickedChallenge.id,
+                competenceId: skill.competenceId,
+                associatedSkillName: skill.name,
+                associatedSkillId: skill.id
+              })
+            );
+            pickedCertificationChallengesByCompetenceId[competenceId] = alreadyPickedChallengesByCompetence;
+          }
+        }
+      });
+    });
+    return _.flatMap(pickedCertificationChallengesByCompetenceId, (challenges) => challenges);
+  }
+
+  _hasChallengeAlreadyBeenPicked(alreadyPickedChallengesByCompetence, pickedChallenge) {
+    return alreadyPickedChallengesByCompetence.map((pickedChallenges) => pickedChallenges.challengeId).includes(pickedChallenge.id);
+  }
+
+  _pickRandomChallengeForSkill(skill) {
+    const unansweredChallenges = this._unansweredChallengesForSkill(skill);
+    let pickedChallenge = undefined;
+    if (!_.isEmpty(unansweredChallenges)) {
+      pickedChallenge = this._pickRandomChallengeFrom(unansweredChallenges);
+    } else {
+      pickedChallenge = this._pickRandomChallengeFrom(skill.challenges);
+    }
+    return pickedChallenge;
+  }
+
+  _pickRandomChallengeFrom(challenges) {
+    return _.sample(challenges);
+  }
+
+  _unansweredChallengesForSkill(skill) {
+    return skill.challenges.filter((challenge) => !challenge.hasBeenCorrectlyAnsweredByCandidateDuringPlacement);
+  }
+
+  static from({
+    allCorrectlyAnsweredChallengeIds,
+    allFrenchFranceOperativeChallenges,
+    pixCompetences,
+    placementProfile
+  }) {
+    const allCertificationChallenges = this._allCertificationChallenges(pixCompetences, allFrenchFranceOperativeChallenges);
+    const certificationChallengesCorrectlyAnsweredByCandidate = this._certificationChallengesCorrectlyAnsweredByCandidate(allCertificationChallenges, allCorrectlyAnsweredChallengeIds);
+    const certifiableCompetenceIds = this._certifiableCompetenceIds(placementProfile);
+    const testableSkills = this._testableSkills(certificationChallengesCorrectlyAnsweredByCandidate, certifiableCompetenceIds);
+
+    const skillsByCompetenceIdOrderedByDescendingSkillDifficulty = this._groupByCompetenceIdAndDescendingDifficulty(testableSkills);
+
+    const challengesBySkillByCompetenceId = this._populateWithCertificationChallenges(
+      skillsByCompetenceIdOrderedByDescendingSkillDifficulty,
+      allCertificationChallenges,
+      allCorrectlyAnsweredChallengeIds
+    );
+
+    return new LearningContentForCertificationCandidate(challengesBySkillByCompetenceId);
+  }
+
+  static _populateWithCertificationChallenges(skillsByCompetenceIdOrderedByDescendingSkillDifficulty, allCertificationChallenges, allCorrectlyAnsweredChallengeIds) {
+    return _(skillsByCompetenceIdOrderedByDescendingSkillDifficulty)
+      .mapValues((skills) => skills.map((skill) => {
+        const mutatedSkill = _.cloneDeep(skill);
+        const skillRelatedChallenges = _.filter(allCertificationChallenges, (challenge) => _.map(challenge.skills, 'id').includes(mutatedSkill.id));
+        const skillRelatedChallengesWithIsAnsweredFlag = _.map(skillRelatedChallenges, (challenge) => {
+          const mutatedChallenge = _.cloneDeep(challenge);
+          mutatedChallenge.hasBeenCorrectlyAnsweredByCandidateDuringPlacement = _.some(allCorrectlyAnsweredChallengeIds, (correctlyAnsweredChallengeId) => correctlyAnsweredChallengeId === challenge.id);
+          return mutatedChallenge;
+        });
+        mutatedSkill.challenges = skillRelatedChallengesWithIsAnsweredFlag;
+        return mutatedSkill;
+      })).value();
+  }
+
+  static _groupByCompetenceIdAndDescendingDifficulty(testableSkills) {
+    return _(testableSkills)
+      .groupBy((skill) => skill.competenceId)
+      .mapValues((skills) => _.orderBy(skills, ['difficulty'], ['desc']))
+      .value();
+  }
+
+  static _testableSkills(certificationChallengesCorrectlyAnsweredByCandidate, certifiableCompetenceIds) {
+    return _(certificationChallengesCorrectlyAnsweredByCandidate)
+      .flatMap((challenge) => challenge.skills)
+      .uniqBy((skill) => skill.id)
+      .filter((skill) => certifiableCompetenceIds.includes(skill.competenceId))
+      .value();
+  }
+
+  static _certifiableCompetenceIds(placementProfile) {
+    return _(placementProfile.userCompetences)
+      .filter((userCompetence) => userCompetence.isCertifiable())
+      .map((userCompetence) => userCompetence.id)
+      .value();
+  }
+
+  static _certificationChallengesCorrectlyAnsweredByCandidate(allCertificationChallenges, allCorrectlyAnsweredChallengeIds) {
+    const certificationChallengesCorrectlyAnsweredByCandidate = _(allCertificationChallenges)
+      .filter((challenge) => allCorrectlyAnsweredChallengeIds.includes(challenge.id))
+      .value();
+    return certificationChallengesCorrectlyAnsweredByCandidate;
+  }
+
+  // TODO : move to challengeRepository.findFrenchFranceOperativeByCompetenceIds([competenceIds])
+  static _allCertificationChallenges(pixCompetences, allFrenchFranceOperativeChallenges) {
+    const pixCompetenceIds = pixCompetences.map((pixCompetence) => pixCompetence.id);
+    const allCertificationChallenges = _(allFrenchFranceOperativeChallenges)
+      .filter((challenge) => pixCompetenceIds.includes(challenge.competenceId))
+      .value();
+    return allCertificationChallenges;
+  }
 }
 
-function _getUserCompetenceByChallengeCompetenceId(userCompetences, challenge) {
-  return userCompetences.find((userCompetence) => userCompetence.id === challenge.competenceId);
-}
-
-function _skillHasAtLeastOneChallenge(skill, challenges) {
-  const challengesBySkill = Challenge.findBySkill({ challenges, skill });
-  return challengesBySkill.length > 0;
-}
