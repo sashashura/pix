19a906ae70220415c01c2a45e7ce4418420ee57c
Etienne GIROT <egirot@octo.com>
2020-08-21 14:56:27 +0200

    TCR: lint altered files only

--- api/scripts/tcr/tcr.sh
+++ api/scripts/tcr/tcr.sh
@@ -1,11 +1,18 @@
-if npm run lint:fix; then
-  if npm run test:api:path $@ -- --bail; then
-    echo "TCR => COMMIT"
-    git commit -am "TCR:WIP"
+alteredFilePaths=$(git ls-files -m '*.js');
+
+if [ ! -z "$alteredFilePaths" ] ; then
+  if eslint $alteredFilePaths --fix ; then
+    if npm run test:api:path $@ -- --bail; then
+      echo "TCR => COMMIT"
+      git commit -am "TCR:WIP"
+    else
+      echo "TCR => REVERT"
+      git checkout HEAD lib/
+    fi
   else
-    echo "TCR => REVERT"
-    git checkout HEAD lib/
+    echo "TCR => ABORT"
   fi
 else
-  echo "TCR => ABORT"
+  echo "TCR => NOTHING HAS CHANGED"
 fi
+
