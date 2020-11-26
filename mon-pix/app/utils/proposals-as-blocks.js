4827232de427a43cd693f2f0cb74cfcf512cdd45
celineung <celine.ung@zenika.com>
2020-11-26 16:45:58 +0100

    Refacto proposalAsBlocks according to step-down-rule

--- mon-pix/app/utils/proposals-as-blocks.js
+++ mon-pix/app/utils/proposals-as-blocks.js
@@ -1,79 +1,6 @@
 import isEmpty from 'lodash/isEmpty';
 import ResponseBlock from './response-block';
 
-function parseInput(isInput, input) {
-  let block;
-
-  switch (input) {
-    case '${':
-      isInput = true;
-      break;
-    case undefined:
-      isInput = false;
-      break;
-    case '':
-      break;
-    default:
-      if (isInput) {
-        block = new ResponseBlock({ input });
-      }
-      else {
-        block = new ResponseBlock({ text: input });
-      }
-  }
-
-  return { isInput, block };
-}
-
-function isLastElement(currentIdx, elements) {
-  return currentIdx === (elements.length - 1);
-}
-
-function getLastLine(lines) {
-  return lines[lines.length - 1];
-}
-
-function isAriaLabelNeededForInputs(lines) {
-  const lastLine = getLastLine(lines);
-  const inputStartRegex = /\${/g;
-  const lastLineInputs = lastLine.match(inputStartRegex, '');
-  const hasMoreThanOneInputField = lastLineInputs && lastLineInputs.length > 1;
-  const inputRegex = /\s*(\${.+?})|-| /g; //regex that remove spaces, -, ${} => should return only letters
-  const lastLineWithoutInput = lastLine.replace(inputRegex, '');
-  const blocks = lastLine.split(/\s*(\${)|}\s*/);
-  const hasNoTextBeforeInput = !(blocks && blocks[0].replace(inputRegex, '').length > 0);
-
-  if (hasMoreThanOneInputField || hasNoTextBeforeInput) {
-    return true;
-  }
-
-  return lastLineWithoutInput.length === 0;
-}
-
-function buildLineFrom(blocks, ariaLabelNeeded, challengeResponseTemplate) {
-  let previousBlockText = '';
-  for (let blockIdx = 0; blockIdx < blocks.length; blockIdx += 1) {
-    const { isInput, block } = parseInput((isInput || false), blocks[blockIdx]);
-    if (!block) {
-      continue;
-    }
-
-    const isInputField = block.input != null;
-    challengeResponseTemplate.incrementInputCount(isInputField);
-
-    block.attachInputAndPlaceholderIfExist();
-    const didAttachedLabel = block.attachLabel({
-      isInputField,
-      ariaLabelNeeded,
-      previousBlockText,
-      questionIdx: challengeResponseTemplate.inputCount });
-    previousBlockText = didAttachedLabel ? '' : block.text;
-
-    const canAddBlockToTemplate = ariaLabelNeeded || isInputField || isLastElement(blockIdx, blocks);
-    challengeResponseTemplate.add({ canAddBlockToTemplate, block: block.get() });
-  }
-}
-
 class ChallengeResponseTemplate {
 
   constructor() {
@@ -124,4 +51,77 @@ export default function proposalsAsBlocks(proposals) {
     challengeResponseTemplate.addLineBreakIfIsNotLastLine({ lineIdx, lines });
   });
   return challengeResponseTemplate.get();
+}
+
+function isAriaLabelNeededForInputs(lines) {
+  const lastLine = getLastLine(lines);
+  const inputStartRegex = /\${/g;
+  const lastLineInputs = lastLine.match(inputStartRegex, '');
+  const hasMoreThanOneInputField = lastLineInputs && lastLineInputs.length > 1;
+  const inputRegex = /\s*(\${.+?})|-| /g; //regex that remove spaces, -, ${} => should return only letters
+  const lastLineWithoutInput = lastLine.replace(inputRegex, '');
+  const blocks = lastLine.split(/\s*(\${)|}\s*/);
+  const hasNoTextBeforeInput = !(blocks && blocks[0].replace(inputRegex, '').length > 0);
+
+  if (hasMoreThanOneInputField || hasNoTextBeforeInput) {
+    return true;
+  }
+
+  return lastLineWithoutInput.length === 0;
+}
+
+function getLastLine(lines) {
+  return lines[lines.length - 1];
+}
+
+function buildLineFrom(blocks, ariaLabelNeeded, challengeResponseTemplate) {
+  let previousBlockText = '';
+  for (let blockIdx = 0; blockIdx < blocks.length; blockIdx += 1) {
+    const { isInput, block } = parseInput((isInput || false), blocks[blockIdx]);
+    if (!block) {
+      continue;
+    }
+
+    const isInputField = block.input != null;
+    challengeResponseTemplate.incrementInputCount(isInputField);
+
+    block.attachInputAndPlaceholderIfExist();
+    const didAttachedLabel = block.attachLabel({
+      isInputField,
+      ariaLabelNeeded,
+      previousBlockText,
+      questionIdx: challengeResponseTemplate.inputCount });
+    previousBlockText = didAttachedLabel ? '' : block.text;
+
+    const canAddBlockToTemplate = ariaLabelNeeded || isInputField || isLastElement(blockIdx, blocks);
+    challengeResponseTemplate.add({ canAddBlockToTemplate, block: block.get() });
+  }
+}
+
+function parseInput(isInput, input) {
+  let block;
+
+  switch (input) {
+    case '${':
+      isInput = true;
+      break;
+    case undefined:
+      isInput = false;
+      break;
+    case '':
+      break;
+    default:
+      if (isInput) {
+        block = new ResponseBlock({ input });
+      }
+      else {
+        block = new ResponseBlock({ text: input });
+      }
+  }
+
+  return { isInput, block };
+}
+
+function isLastElement(currentIdx, elements) {
+  return currentIdx === (elements.length - 1);
 }
