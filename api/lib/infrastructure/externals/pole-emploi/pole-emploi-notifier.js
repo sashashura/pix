ad704b3fd1bd2f198461972d6f77ec844b92abf2
lisequesnel <quesnel.lise@gmail.com>
2020-12-14 20:31:01 +0100

    [skip ci] WIP

--- api/lib/infrastructure/externals/pole-emploi/pole-emploi-notifier.js
+++ api/lib/infrastructure/externals/pole-emploi/pole-emploi-notifier.js
@@ -7,11 +7,40 @@ const { UnexpectedUserAccount } = require('../../../domain/errors');
 
 module.exports = {
   async notify(userId, payload) {
-    const authenticationMethod = await authenticationMethodRepository.findOneByUserIdAndIdentityProvider({ userId, identityProvider: AuthenticationMethod.identityProviders.POLE_EMPLOI });
+    let authenticationMethod = await authenticationMethodRepository.findOneByUserIdAndIdentityProvider({ userId, identityProvider: AuthenticationMethod.identityProviders.POLE_EMPLOI });
     const accessToken = _.get(authenticationMethod, 'authenticationComplement.accessToken');
     if (!accessToken) {
       throw new UnexpectedUserAccount({ message: 'Le compte utilisateur n\'est pas rattaché à l\'organisation Pôle Emploi' });
     }
+
+    const expiredDate = _.get(authenticationMethod, 'authenticationComplement.expiredDate');
+    const refreshToken = _.get(authenticationMethod, 'authenticationComplement.refreshToken');
+    if (!refreshToken || !expiredDate || expiredDate <= new Date()) {
+      const data = {
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+        client_secret: settings.poleEmploi.clientSecret,
+        client_id: settings.poleEmploi.clientId,
+      };
+
+      try {
+        const response = await axios.post( // idéalement utiliser le httpAgent.post en lui ajoutant un retour `payload`
+          settings.poleEmploi.tokenUrl,
+          querystring.stringify(data),
+          {
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+          },
+        );
+      } catch (error) {
+        return { // pour une raison X ou Y, l'envoi des résultats a échoué
+          isSuccessful: false,
+          code: error.code,
+        };
+      }
+      const authenticationComplement = { truc: response.truc }; // construction à compléter
+      authenticationMethod = await authenticationMethodRepository.updatePoleEmploiAuthenticationComplementByUserId({ authenticationComplement, userId });
+    }
+
     const url = settings.poleEmploi.sendingUrl;
     const headers = {
       'Authorization': `Bearer ${accessToken}`,
