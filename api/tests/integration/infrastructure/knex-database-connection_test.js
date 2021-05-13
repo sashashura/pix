5a845447554ff29df78fecdc9e14f7e8a492359a
Pierre TOP (TOPI) <ptop@octo.com>
2021-05-13 14:52:11 +0200

    [API] Replace unecessary bookshelf reference by knex (struct.)

--- api/tests/integration/infrastructure/knex-database-connection_test.js
+++ api/tests/integration/infrastructure/knex-database-connection_test.js
@@ -1,6 +1,6 @@
 const { expect, databaseBuilder } = require('../../test-helper');
 
-const knexDatabaseConnection = require('../../../db/knex-database-connection');
+const { knex, emptyAllTables } = require('../../../db/knex-database-connection');
 
 const { UserNotFoundError } = require('../../../lib/domain/errors');
 const userRepository = require('../../../lib/infrastructure/repositories/user-repository');
@@ -9,7 +9,7 @@ describe('Integration | Infrastructure | knex-database-connection', () => {
 
   it('should connect to the database', async () => {
     // when
-    const resultSet = await knexDatabaseConnection.knex.raw('SELECT 1 as value');
+    const resultSet = await knex.raw('SELECT 1 as value');
     // then
     expect(resultSet.rows || resultSet).to.deep.equal([{ value: 1 }]);
   });
@@ -20,7 +20,7 @@ describe('Integration | Infrastructure | knex-database-connection', () => {
     await databaseBuilder.commit();
 
     // when
-    await knexDatabaseConnection.emptyAllTables();
+    await emptyAllTables();
 
     // then
     await expect(userRepository.get(id)).to.be.rejectedWith(UserNotFoundError);
