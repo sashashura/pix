f78f4c4d413e874b8c3a9529db40a397b990d8c9
Anne-G <ang.schall@gmail.com>
2021-10-07 16:31:18 +0200

    add cnav config and scripts

---
+++ POC-keycloak/keycloak/export-cnav-config.sh
@@ -0,0 +1,9 @@
+trap "echo stop" INT
+docker-compose exec cnav-idp /opt/jboss/keycloak/bin/standalone.sh \
+  -Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
+  -Dkeycloak.migration.provider=singleFile \
+  -Dkeycloak.migration.realmName=cnav \
+  -Dkeycloak.migration.usersExportStrategy=REALM_FILE \
+  -Dkeycloak.migration.file=/tmp/cnav.json
+
+docker-compose exec cnav-idp cat /tmp/cnav.json > cnav.json
