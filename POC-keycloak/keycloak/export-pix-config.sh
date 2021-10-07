7317ad6b07bc74e08820dd192adfe7199e4e4aee
yschoueri <yasmina.schoueri@gmail.com>
2021-10-07 17:26:02 +0200

    fix export scripts

--- POC-keycloak/keycloak/export-pix-config.sh
+++ POC-keycloak/keycloak/export-pix-config.sh
@@ -1,9 +1,6 @@
-trap "echo stop" INT
 docker-compose exec pix-broker /opt/jboss/keycloak/bin/standalone.sh \
   -Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
   -Dkeycloak.migration.provider=singleFile \
   -Dkeycloak.migration.realmName=pix \
   -Dkeycloak.migration.usersExportStrategy=REALM_FILE \
   -Dkeycloak.migration.file=/tmp/pix.json
-
-docker-compose exec pix-broker cat /tmp/pix.json > pix.json
