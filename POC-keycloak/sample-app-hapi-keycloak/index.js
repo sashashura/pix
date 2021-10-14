25097f95fab151db4ee9679ac2ccef89f8d2171a
Anne-G <ang.schall@gmail.com>
2021-10-14 16:57:34 +0200

    Add HapiPix client and npm start scrip

--- POC-keycloak/sample-app-hapi-keycloak/index.js
+++ POC-keycloak/sample-app-hapi-keycloak/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const authKeycloak = require('hapi-auth-keycloak');
 
 const init = async () => {
 
@@ -9,20 +10,37 @@ const init = async () => {
     host: 'localhost',
   });
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: () => {
-      return 'Hello World!';
-    },
+  await server.register({ plugin: authKeycloak });
+
+  server.auth.strategy('keycloak-jwt', 'keycloak-jwt', {
+    realmUrl: 'http://localhost:8080/auth/realms/pix',
+    clientId: 'hapiPix',
+    minTimeBetweenJwksRequests: 15,
+    cache: true,
+    userInfo: ['name', 'email'],
   });
+
+  server.route([
+    {
+      method: 'GET',
+      path: '/check-sso',
+      config: {
+        description: 'protected endpoint',
+        auth: {
+          strategies: ['keycloak-jwt'],
+        },
+        handler() {
+          return 'hello world';
+        },
+      },
+    },
+  ]);
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
   console.log(err);
   process.exit(1);
 });
