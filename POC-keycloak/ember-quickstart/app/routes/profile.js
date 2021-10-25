781f1e53f5b4c663ac1a8c37b4d676a0ef442a24
Anne-G <ang.schall@gmail.com>
2021-10-25 17:02:22 +0200

    protect profile route

--- POC-keycloak/ember-quickstart/app/routes/profile.js
+++ POC-keycloak/ember-quickstart/app/routes/profile.js
@@ -1,6 +1,7 @@
-import OIDCApplicationRouteMixin from 'ember-simple-auth-oidc/mixins/oidc-application-route-mixin';
+// import OIDCApplicationRouteMixin from 'ember-simple-auth-oidc/mixins/oidc-application-route-mixin';
 import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default class ProfileRoute extends Route.extend(
-  OIDCApplicationRouteMixin
+  AuthenticatedRouteMixin
 ) {}
