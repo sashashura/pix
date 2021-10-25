c6b9bbcc9889e6aab8961bab397ff9199486042d
Anne-G <ang.schall@gmail.com>
2021-10-25 16:52:39 +0200

    add application route & lint

---
+++ POC-keycloak/ember-quickstart/app/routes/application.js
@@ -0,0 +1,6 @@
+import Route from '@ember/routing/route';
+import OIDCApplicationRouteMixin from 'ember-simple-auth-oidc/mixins/oidc-application-route-mixin';
+
+export default class ApplicationRoute extends Route.extend(
+  OIDCApplicationRouteMixin
+) {}
--- POC-keycloak/ember-quickstart/app/routes/profile.js
+++ POC-keycloak/ember-quickstart/app/routes/profile.js
@@ -1,3 +1,6 @@
+import OIDCApplicationRouteMixin from 'ember-simple-auth-oidc/mixins/oidc-application-route-mixin';
 import Route from '@ember/routing/route';
 
-export default class ProfileRoute extends Route {}
+export default class ProfileRoute extends Route.extend(
+  OIDCApplicationRouteMixin
+) {}
