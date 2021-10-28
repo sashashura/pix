56dc147746e87a10e80e1ecb91d9f823932e8ade
Anne-G <ang.schall@gmail.com>
2021-10-28 16:13:21 +0200

    add deconnection button

---
+++ POC-keycloak/ember-quickstart/app/controllers/application.js
@@ -0,0 +1,16 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class ApplicationController extends Controller {
+  @service session;
+
+  get isLogged() {
+    return this.session.isAuthenticated;
+  }
+
+  @action
+  deco() {
+    this.session.invalidate();
+  }
+}
