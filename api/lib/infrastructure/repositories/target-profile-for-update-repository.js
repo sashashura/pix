cc9f77f53c66730263df6e423eca90fe13d8c630
yaelle <ymarsiac@octo.com>
2021-11-25 09:35:02 +0100

    [API] create targetProfileForUpdate repository

---
+++ api/lib/infrastructure/repositories/target-profile-for-update-repository.js
@@ -0,0 +1,19 @@
+const targetProfileForUpdate = require('../../domain/models/TargetProfileForUpdate');
+const { knex } = require('../bookshelf');
+const { NotFoundError } = require('../../domain/errors');
+
+module.exports = {
+  async get(id) {
+    const row = await knex('target-profiles').select('id', 'name', 'description', 'comment').where({ id }).first();
+
+    if (!row) {
+      throw new NotFoundError(`Le profil cible avec l'id ${id} n'existe pas`);
+    }
+
+    return new targetProfileForUpdate(row);
+  },
+
+  async update(targetProfile) {
+    return await knex('target-profiles').where({ id: targetProfile.id }).update(targetProfile);
+  },
+};
