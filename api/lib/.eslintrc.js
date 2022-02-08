22fe0023e99cce9b62ff9f66503b9bceb295d0f2
Pierre TOP (TOPI) <ptop@octo.com>
2022-02-08 19:50:24 +0100

    Activate rule for use-cases

--- api/lib/.eslintrc.js
+++ api/lib/.eslintrc.js
@@ -1,3 +1,4 @@
+const path = require('path');
 module.exports = {
   extends: '../.eslintrc.yaml',
   rules: {
@@ -13,4 +14,21 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: 'domain/usecases/**',
+      rules: {
+        'node/no-restricted-require': [
+          'error',
+          [
+            {
+              name: path.resolve(__dirname, 'infrastructure/**'),
+              message:
+                'Do no refer directly to infrastructure in use-cases, refer do it indirectly by passing reference in arguments from the controller',
+            },
+          ],
+        ],
+      },
+    },
+  ],
 };
