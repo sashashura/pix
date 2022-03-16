6c3c28721225f4aaf5e5a147b8a88f78e98fb8c5
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-03-16 16:33:43 +0100

    feat: add optional banner config in feature toggle serializer

--- api/lib/infrastructure/serializers/jsonapi/feature-toggle-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/feature-toggle-serializer.js
@@ -1,12 +1,12 @@
 const { Serializer } = require('jsonapi-serializer');
 
 module.exports = {
-  serialize(features) {
+  serialize(config) {
     return new Serializer('feature-toggles', {
-      transform(features) {
-        return { id: 0, ...features };
+      transform(config) {
+        return { id: 0, ...config };
       },
-      attributes: Object.keys(features),
-    }).serialize(features);
+      attributes: Object.keys(config),
+    }).serialize(config);
   },
 };
--- api/tests/unit/infrastructure/serializers/jsonapi/feature-toggle-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/feature-toggle-serializer_test.js
@@ -3,26 +3,62 @@ const serializer = require('../../../../../lib/infrastructure/serializers/jsonap
 
 describe('Unit | Serializer | JSONAPI | feature-toggle-serializer', function () {
   describe('#serialize', function () {
-    it('should convert feature-toggle object into JSON API data', function () {
-      // given
-      const featureToggles = {
-        someFeatureToggle: true,
-      };
-      const expectedJSON = {
-        data: {
-          type: 'feature-toggles',
-          id: '0',
-          attributes: {
-            'some-feature-toggle': true,
+    describe('if there is no banner', () => {
+      it('should convert feature-toggle object into JSON API data', function () {
+        // given
+        const featureToggles = {
+          someFeatureToggle: true,
+        };
+        const expectedJSON = {
+          data: {
+            type: 'feature-toggles',
+            id: '0',
+            attributes: {
+              'some-feature-toggle': true,
+            },
           },
-        },
-      };
+        };
 
-      // when
-      const json = serializer.serialize(featureToggles);
+        // when
+        const json = serializer.serialize(featureToggles);
 
-      // then
-      expect(json).to.deep.equal(expectedJSON);
+        // then
+        expect(json).to.deep.equal(expectedJSON);
+      });
+    });
+
+    describe('if there is one banner', () => {
+      it('should convert config object into JSON API data', function () {
+        // given
+        const featureToggles = {
+          someFeatureToggle: true,
+          banner: {
+            type: 'warning',
+            message:
+              'Il est possible que votre expérience sur Pix soit dégradée suite à des perturbations. Le problème est en cours de résolution. Merci de votre compréhension.',
+          },
+        };
+        const expectedJSON = {
+          data: {
+            type: 'feature-toggles',
+            id: '0',
+            attributes: {
+              'some-feature-toggle': true,
+              banner: {
+                type: 'warning',
+                message:
+                  'Il est possible que votre expérience sur Pix soit dégradée suite à des perturbations. Le problème est en cours de résolution. Merci de votre compréhension.',
+              },
+            },
+          },
+        };
+
+        // when
+        const json = serializer.serialize(featureToggles);
+
+        // then
+        expect(json).to.deep.equal(expectedJSON);
+      });
     });
   });
 });
